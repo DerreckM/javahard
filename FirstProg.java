450e902fde824f9a499a6df5fd77c6c7a6ba80c8
derreck <mansheim23@hotmail.com>
2016-08-17 23:36:25 -0500

    wrote and compiled first java program

--- FirstProg.java
+++ FirstProg.java
@@ -1,3 +1,6 @@
 public class FirstProg {
-
+  public static void main( String[] args ) {
+    System.out.println( "I am Derreck the amazing." );
+    System.out.println( "Today's date is August 17, 2016." );
+  }
 }
